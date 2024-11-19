dc7c41c431acaa04ea283282754bd5d7ac0bcb2a
shakarim <shakarimsapa@gmail.com>
2024-11-19 06:28:07 +0500

    solve hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(receivedString string) (string, error) {
+	if receivedString == "" {
+		return "", nil
+	}
+
+	var result []string = make([]string, len(receivedString))
+	var currentLetter string = ""
+
+	for _, char := range receivedString {
+		if unicode.IsDigit(char) {
+			if currentLetter == "" {
+				return "", ErrInvalidString
+			}
+			intValue, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			result = append(result, strings.Repeat(string(currentLetter), intValue))
+			currentLetter = ""
+		} else {
+			if currentLetter != "" {
+				result = append(result, string(currentLetter))
+			}
+			currentLetter = string(char)
+		}
+	}
+
+	if currentLetter != "" {
+		result = append(result, string(currentLetter))
+	}
+
+	return strings.Join(result, ""), nil
 }
