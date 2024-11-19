7eedd95071a68de83d4410b1bb142802dc4e9420
shakarim <shakarimsapa@gmail.com>
2024-11-19 07:58:33 +0500

    solve hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,46 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(sourceValue string) []string {
 	// Place your code here.
-	return nil
+	var words []string = strings.Fields(sourceValue)
+	var numberOfDash = strings.Count(sourceValue, "-")
+	var result map[string]int = make(map[string]int, len(words)+(numberOfDash*2))
+
+	for _, word := range words {
+		indicateWordInMap(result, word)
+	}
+
+	keys := make([]string, 0, len(result))
+
+	for key := range result {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return result[keys[i]] > result[keys[j]]
+	})
+
+	if len(keys) >= 10 {
+		return keys[:10]
+	}
+
+	return keys
+}
+
+func indicateWordInMap(result map[string]int, word string) {
+	if word != "" {
+		val, ok := result[word]
+		if ok {
+			result[word] = val + 1
+		} else {
+			result[word] = 1
+		}
+	}
 }
